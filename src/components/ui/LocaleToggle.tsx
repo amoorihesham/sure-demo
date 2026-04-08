ff087f2891b21ec62ac1832b46bb1ec08e2c2c0c
Amr_Hesham <amrheshammacc@gmail.com>
2026-04-08 14:12:48 +0200

    locale

--- src/components/ui/LocaleToggle.tsx
+++ src/components/ui/LocaleToggle.tsx
@@ -1,7 +1,19 @@
-import React from "react";
-import { Button } from "./button";
+import React, { useState } from "react";
+import { Button, buttonVariants } from "./button";
+import { Globe } from "lucide-react";
 
-export function LocaleToggle(){
-    const [locale, setLocale] = React.useState<"en" | "ar">(()=>window.location.pathname.split('/')[1] as "en" | "ar");
-    return <Button>{locale}</Button>
+export function LocaleToggle() {
+  const [locale, setLocale] = useState(() => window.location.pathname.split("/")[1]);
+  const url = locale === "en" ? "/ar" : "/en";
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => (window.location.href = url)}
+      className={buttonVariants({ variant: "outline", size: "icon", className: "text-white" })}
+    >
+      <Globe className="size-4" />
+    </Button>
+  );
 }
