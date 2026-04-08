5a7e4230c24b56c595bdf8ac9633063093a5fff0
Amr_Hesham <amrheshammacc@gmail.com>
2026-04-08 16:19:23 +0200

    proto-type

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   i18n: {
-    locales: ["ar", "en"],
-    defaultLocale: "en",
+    locales: ['ar', 'en'],
+    defaultLocale: 'en',
     routing: {
       prefixDefaultLocale: true,
       redirectToDefaultLocale: true,
--- src/components/ui/LocaleToggle.tsx
+++ src/components/ui/LocaleToggle.tsx
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
-import { Button, buttonVariants } from "./button";
-import { Globe } from "lucide-react";
+import React, { useState } from 'react';
+import { Button, buttonVariants } from './button';
+import { Globe } from 'lucide-react';
 
 export function LocaleToggle() {
-  const [locale, setLocale] = useState(() => window.location.pathname.split("/")[1]);
-  const url = locale === "en" ? "/ar" : "/en";
+  const [locale, setLocale] = useState(
+    () => window.location.pathname.split('/')[1]
+  );
+  const url = locale === 'en' ? '/ar' : '/en';
 
   return (
     <Button
       variant="outline"
       size="icon"
       onClick={() => (window.location.href = url)}
-      className={buttonVariants({ variant: "outline", size: "icon", className: "text-white" })}
+      className={buttonVariants({
+        variant: 'outline',
+        size: 'icon',
+        className: 'text-white',
+      })}
     >
       <Globe className="size-4" />
     </Button>
